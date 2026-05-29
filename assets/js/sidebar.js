c5ae58b74a7898f06029446dec950cb9ce7f3829
matts-maps <55251480+matts-maps@users.noreply.github.com>
2026-05-30 00:06:40 +0800

    Code refactoring

--- assets/js/sidebar.js
+++
@@ -1,6 +0,0 @@
-const sidebar = document.querySelector("[data-sidebar]");
-const toggle = document.querySelector("[data-sidebar-toggle]");
-
-toggle.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-});
